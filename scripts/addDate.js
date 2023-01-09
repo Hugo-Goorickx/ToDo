d1e4b4dada36252d079d5494c24aea2906cfa87d
Hugo Goorickx <hugo@goorickx.be>
2023-01-09 13:53:58 +0100

    Correction du retunr

--- scripts/addDate.js
+++ scripts/addDate.js
@@ -1,33 +1,33 @@
 import { updateDatesEventAPI } from "./manipsAPI.js";
 
 export async function  addDate(id){
-let day = prompt ("Ajoute le jour (jj)");
-if (day > 31){
-    alert ('au format (dd)');
+    let day = prompt ("Ajoute le jour (jj)");
+    if (day > 31){
+        alert ('au format (dd)');
+        return
+    }
+
+    let month = prompt ("Ajoute le mois (mm)");
+    if (month > 12){
+    alert ('au format (mm)');
     return
-}
+    }
 
-let month = prompt ("Ajoute le mois (mm)");
-if (month > 12){
-alert ('au format (mm)');
-return
-}
-
-let years = prompt ("Ajoute le jour (aaaa)");
-if (years < 2023 || years > 2100){
-alert ('au format (aaaa)');
-retunr
-}
+    let years = prompt ("Ajoute le jour (aaaa)");
+    if (years < 2023 || years > 2100){
+    alert ('au format (aaaa)');
+    return
+    }
 
 
-let date = years +'-'+ month + '-' + day;
+    let date = years +'-'+ month + '-' + day;
 
-let confirmDate = confirm ('Voulez-vous vraiment ajouter cette date?');
+    let confirmDate = confirm ('Voulez-vous vraiment ajouter cette date?');
 
-date ? confirmDate : '';
+    date ? confirmDate : '';
 
 
-confirmDate ? await updateDatesEventAPI(id,{"dates": [date]}) : '';
+    confirmDate ? await updateDatesEventAPI(id,{"dates": [date]}) : '';
 
 }
 
